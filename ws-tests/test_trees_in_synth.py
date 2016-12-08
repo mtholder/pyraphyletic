bf208cd4ad8542f3eb719494e2ba0e5e5060f82f
Mark T. Holder <mtholder@gmail.com>
2016-12-08 12:42:39 -0600

    test of trees in synth

---
+++ ws-tests/test_trees_in_synth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import sys, os
+from opentreetesting import test_http_json_method, config
+DOMAIN = config('host', 'apihost')
+SUBMIT_URI = DOMAIN + '/v3/trees_in_synth'
+r = test_http_json_method(SUBMIT_URI,
+                          'GET',
+                          expected_status=200,
+                          return_bool_data=True)
+if not r[0]:
+    sys.stderr.write("Note that the test trees_in_synth.py will fail if your test collections repo does not have " \
+                     "collections that have the same IDs as the collections currently used in synthesis.\n")
+    sys.exit(1)
+#print(r[1])
